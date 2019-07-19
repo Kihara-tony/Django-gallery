52e513eccd56946119ba148536fe8d062a544295
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-19 22:46:33 +0300

    added url patterns in the urls of the firstapp.

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,3 +2,10 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.gallery_of_day,name = 'gallery_of_day'),
+    url(r'^search/',views.search_results,name = 'search_results'),
+    url(r'^categories/',views.display_images_categories,name = 'categories'),
+    url(r'^locations/',views.display_images_locations,name= 'locations')
+]
