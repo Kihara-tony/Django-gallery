94d9b5faeaf2fc37816d76780ec223f534fcff59
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-19 22:56:29 +0300

    added class category and functions for saving,deleting and updating category in models.py.

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        Category.objects.filter(id = self.pk).delete()
+    def update_category(self,**kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
