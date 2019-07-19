275934ec73815d7a305e5493ded11d466737badc
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-19 22:58:42 +0300

    added class image and functions for saving,deleting and updating image in models.py.

--- firstapp/models.py
+++ firstapp/models.py
@@ -22,3 +22,18 @@ class Location(models.Model):
         self.objects.filter(id = self.pk).delete(**kwargs)
     def update_location(self,**kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
+        
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'pics/',null =True)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    category = models.ForeignKey('Category',on_delete =models.CASCADE,null='True',blank=True)
+    location =models.ForeignKey('Location',on_delete= models.CASCADE,null='True',blank=True)
+    def __str__(self):
+        return self.name
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        image.objects.filter(id =self.pk).delete()
+    def update_image(self,**kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
