c0cca855b6ecd433373de9f2f2a020e29cbb0533
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-19 23:46:43 +0300

    added a static folder added styling files and its contents.

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'firstapp'
-     'bootstrap3'
+    'firstapp',
+     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
