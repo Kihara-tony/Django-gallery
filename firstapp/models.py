9391bc397481d42f11ae3ca8f9c0a420139835c2
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-19 22:57:33 +0300

    added class location and functions for saving,deleting and updating location in models.py.

--- firstapp/models.py
+++ firstapp/models.py
@@ -11,3 +11,14 @@ class Category(models.Model):
         Category.objects.filter(id = self.pk).delete()
     def update_category(self,**kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
+        
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    def save_location(self):
+        self.save()
+    def delete_location(self,**kwargs):
+        self.objects.filter(id = self.pk).delete(**kwargs)
+    def update_location(self,**kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
