0f92416814acaef80fcf953ea948acae29e64ec8
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-22 15:56:01 +0300

    added some updates in the models.

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=150)
     def __str__(self):
@@ -15,10 +15,13 @@ class Location(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
+    
     def save_location(self):
         self.save()
+        
     def delete_location(self,**kwargs):
         self.objects.filter(id = self.pk).delete(**kwargs)
+        
     def update_location(self,**kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
         
@@ -31,10 +34,13 @@ class Image(models.Model):
     location =models.ForeignKey('Location',on_delete= models.CASCADE,null='True',blank=True)
     def __str__(self):
         return self.name
+    
     def save_image(self):
         self.save()
+        
     def delete_image(self):
         image.objects.filter(id =self.pk).delete()
+        
     def update_image(self,**kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
         
